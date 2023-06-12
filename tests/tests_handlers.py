1b4982e1a37b6196c759b32bc22401dd88399bc4
blaushild <3betpush@gmail.com>
2023-06-11 21:09:07 -0400

    Added tests for prepare_string() and prepare_row() from handlers

--- tests/tests_handlers.py
+++ tests/tests_handlers.py
@@ -2,10 +2,12 @@ import unittest
 import os
 import sys
 
-# flake8: noqa
+# flake8: noqabu
 sys.path.append(os.getcwd())
 from handlers import (
     build_sku_category,
+    prepare_string,
+    prepare_row,
 )
 
 
@@ -42,6 +44,74 @@ class TestBuildSkuCategory(unittest.TestCase):
         expected_result = "Товары|Для животных|Корма для животных|Корма для кошек"
 
         result = build_sku_category(product)
+
+        self.assertEqual(result, expected_result)
+
+
+class TestPrepareString(unittest.TestCase):
+    def test_prepare_string_empty_text(self):
+        text = ""
+        expected_result = None
+
+        result = prepare_string(text)
+
+        self.assertEqual(result, expected_result)
+
+    def test_prepare_string_no_special_characters(self):
+        text = "Hello World"
+        expected_result = '"Hello World"'
+
+        result = prepare_string(text)
+
+        self.assertEqual(result, expected_result)
+
+    def test_prepare_string_with_special_characters(self):
+        text = "Hello\nWorld\t\r\xa0"
+        expected_result = '"HelloWorld"'
+
+        result = prepare_string(text)
+
+        self.assertEqual(result, expected_result)
+
+    def test_prepare_string_with_quotes(self):
+        text = 'Hello "World"'
+        expected_result = '"Hello ""World"""'
+
+        result = prepare_string(text)
+
+        self.assertEqual(result, expected_result)
+
+
+class TestPrepareRow(unittest.TestCase):
+    def test_prepare_row_with_strings(self):
+        row = ['Hello', 'World', '123']
+        expected_result = ['"Hello"', '"World"', '"123"']
+
+        result = prepare_row(row)
+
+        self.assertEqual(result, expected_result)
+
+    def test_prepare_row_with_empty_strings(self):
+        row = ['', '', '']
+        expected_result = [None, None, None]
+
+        result = prepare_row(row)
+
+        self.assertEqual(result, expected_result)
+
+    def test_prepare_row_with_mixed_values(self):
+        row = ['Hello', 123, 'World']
+        expected_result = ['"Hello"', 123, '"World"']
+
+        result = prepare_row(row)
+
+        self.assertEqual(result, expected_result)
+
+    def test_prepare_row_with_special_characters(self):
+        row = ['Hello\nWorld', '\t123\r', '']
+        expected_result = ['"HelloWorld"', '"123"', None]
+
+        result = prepare_row(row)
 
         self.assertEqual(result, expected_result)
 
